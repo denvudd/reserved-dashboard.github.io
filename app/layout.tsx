bb34bebb3d3571bc1c5f7b0b531efa5ea5c3ae81
Dima <dimontv7@gmail.com>
2023-08-24 10:06:38 +0300

    basic setup, clerk auth setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Dashboard - Reserved",
+  description: "Admin Dashboard for control your store.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <ClerkProvider>
+        <body className={inter.className}>
+          <main>{children}</main>
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
