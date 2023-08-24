9883e70001ee3694b9011a9bf4e7fe07152c6599
Dima <dimontv7@gmail.com>
2023-08-24 10:55:28 +0300

    create use-store-modal hook, setup zustand, fix npm-packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import './globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { DialogHeader } from "@/components/ui/Dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/Dialog";
+
 export default function Home() {
-  return <p>Hello Admin</p>;
+  return (
+    <Dialog open onOpenChange={() => {}}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Test</DialogTitle>
+          <DialogDescription>Descr</DialogDescription>
+        </DialogHeader>
+        Clidren
+      </DialogContent>
+    </Dialog>
+  );
 }
