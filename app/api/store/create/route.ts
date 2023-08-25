d4587167c1ba58b4471f356a05e4911b42714621
Dima <dimontv7@gmail.com>
2023-08-25 13:42:26 +0300

    create store/create API route

---
+++ app/api/store/create/route.ts
@@ -0,0 +1,34 @@
+import db from "@/lib/prisma";
+import { CreateStoreValidator } from "@/lib/validators/store";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = CreateStoreValidator.parse(body);
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const store = await db.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+  } catch (error) {
+    // DEV
+    console.log("[STORE CREATE]:", error);
+
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
